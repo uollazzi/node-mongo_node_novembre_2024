dd386a13db0a689b72a6f5efe439eedb0d01c298
Matteo Valenzi <dilloawallace@hotmail.com>
2024-11-13 21:17:02 +0100

    .

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log("App Works!");
+const connectionString = "mongodb+srv://uollazzi:uollazzi@cluster0.axxoeci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(connectionString);
+
+client.connect()
+    .then(c => {
+        console.log("Connessione avvenuta con successo.");
+
+        // recupero i dati
+
+        c.close()
+            .then(() => {
+                console.log("Connessione chiusa con successo.");
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    })
+    .catch(err => {
+        console.log("Connessione fallita.");
+        console.log(err);
+    });
+
+console.log("FINITO");
